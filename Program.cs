dc796cb5cae0bdb8d8b559d7f7fcf069d885b3b1
verenstechworld <kenolyoveren@gmail.com>
2024-10-15 00:52:52 +0200

    ip1

--- Program.cs
+++ Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
 
-namespace OddToEvenConverter
+class OddToEvenConverter
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Welcome to the Odd to Even Converter!");
+            Console.Clear();
+            Console.WriteLine("Odd to Even Converter\n");
+            Console.Write("Enter a number: ");
 
-            // Read input from the user
-            Console.Write("Please enter an integer: ");
-            string input = Console.ReadLine();
-
-            // Try to parse the input
-            if (int.TryParse(input, out int number))
+            if (int.TryParse(Console.ReadLine(), out int number))
             {
-                // Check if the number is odd
-                if (number % 2 != 0)
-                {
-                    // Convert odd to even
-                    number += 1; // Increment to make it even
-                    Console.WriteLine($"Converted odd number to even: {number}");
-                }
-                else
-                {
-                    Console.WriteLine($"The number is already even: {number}");
-                }
+                int converted = ConvertOddToEven(number);
+                Console.WriteLine($"\n{number} has been converted to {converted}\n");
             }
             else
             {
-                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                Console.WriteLine("\nInvalid input. Please enter a valid integer.\n");
             }
 
-            // Wait for user to press a key before closing
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            Console.Write("Do you want to convert another number? (y/n): ");
+            if (Console.ReadKey().Key != ConsoleKey.Y)
+            {
+                break;
+            }
         }
+    }
+
+    static int ConvertOddToEven(int num)
+    {
+        if (num % 2 != 0)
+        {
+            return num + 1;
+        }
+        return num;
     }
 }
